245009c9f94325ad601d9999122845878f591420
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-02-02 22:37:09 +0530

    Update and rename SnakeandLadder.java to startposition.java

---
+++ startposition.java
@@ -0,0 +1,18 @@
+public class Start_Position {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome To Snake and Ladder Simulator .... ");
+        System.out.println("************************************");
+
+       
+        int StartPosition = 0;
+        int CurrentPosition = 0;
+
+        
+        System.out.println("Player is Starting at: " + StartPosition);
+
+
+	}
+
+}
